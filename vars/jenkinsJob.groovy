190e3575eadb5bfe7190a9295d44bfc5ffd7b793
Venkata Gummadi <gummadic@gmail.com>
2018-10-15 12:15:03 -0700

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -13,6 +13,28 @@ def call(){
     }
 }
 
+
+@NonCPS
+String createTempLocation(String path) {
+  String tmpDir = pwd tmp: true
+  return tmpDir + File.separator + new File(path).getName()
+}
+
+/**
+  * Returns the path to a temp location of a script from the global library (resources/ subdirectory)
+  *
+  * @param srcPath path within the resources/ subdirectory of this repo
+  * @param destPath destination path (optional)
+  * @return path to local file
+  */
+String copyGlobalLibraryScript(String srcPath, String destPath = null) {
+  destPath = destPath ?: createTempLocation(srcPath)
+  writeFile file: destPath, text: libraryResource(srcPath)
+  echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
+  return destPath
+}
+
+
 def packageArtifact(){
     stage("Package artifact") {
         sh "mvn package"
@@ -21,6 +43,7 @@ def packageArtifact(){
 
 def buildAndTest(){
     stage("Backend tests"){
+        sh(copyGlobalLibraryScript('test.sh'))
         sh "mvn test"
     }
 }
